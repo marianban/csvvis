03a0be88d8d7c819e5394f60f404c819c061431b
Marian Ban <marian.ban@gmail.com>
2020-07-02 18:29:44 +0200

    added typescript + basic styles

---
+++ web-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './colors.scss';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header"></header>
+      <div className="sidebar"></div>
+      <main className="main"></main>
+    </div>
+  );
+}
+
+export default App;
