601c33b97f4dd622acbd728a91025786c6398aff
Marian Ban <marian.ban@gmail.com>
2020-07-02 19:54:04 +0200

    basic styling

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import './colors.scss';
 import './App.scss';
 
 function App() {
   return (
     <div className="app">
       <header className="header"></header>
-      <div className="sidebar"></div>
+      <div className="sidebar">
+        <nav className="nav">
+          <ul>
+            <li></li>
+            <li></li>
+          </ul>
+        </nav>
+      </div>
       <main className="main"></main>
     </div>
   );
--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './reset.scss';
+import './vars.scss';
 import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
