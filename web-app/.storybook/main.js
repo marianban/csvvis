c55289ebdf15f79215a6fa97a5a3037d4a2b8c88
Marian Ban <marian.ban@gmail.com>
2020-07-04 16:13:11 +0200

    added typescript support for storybook

--- web-app/.storybook/main.js
+++ web-app/.storybook/main.js
@@ -1,8 +1,14 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.(ts|tsx|js|jsx)'],
   addons: [
-    '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
   ],
 };
